190ef75d4d7dac16c74da9c3f8f4b50b49869211
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-13 21:14:59 +0300

    new language

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 const taskInput = document.querySelector('.task-input input'),
-filters = document.querySelectorAll(".filters span"),
-clearAll = document.querySelector(".clear-btn"),
-taskBox = document.querySelector(".task-box");
+  filters = document.querySelectorAll(".filters span"),
+  clearAll = document.querySelector(".clear-btn"),
+  taskBox = document.querySelector(".task-box");
 
 let editId;
 let isEditedTask = false;
 
+// получаем localstorage todo-list
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
 filters.forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.querySelector("span.active").classList.remove("active");
-        btn.classList.add("active");
-        showTodo(btn.id);
-    });
+  btn.addEventListener("click", () => {
+    document.querySelector("span.active").classList.remove("active");
+    btn.classList.add("active");
+    showTodo(btn.id);
+  });
 });
 
 function showTodo(filter) {
-    let li = "";
-    if(todos) {
-        todos.forEach((todo, id) => {
-
-            let isCompleted = todo.status == "completed" ? "checked" : "";
-            if(filter == todo.status || filter == "all") {
-                li += `<li class="task">
+  let li = "";
+  if (todos) {
+    todos.forEach((todo, id) => {
+      // если статус todo завершен, устанавливается значение isCompleted в checked
+      let isCompleted = todo.status === "completed" ? "checked" : "";
+      if (filter === todo.status || filter === "all") {
+        li += `<li class="task">
                 <label for="${id}">
                     <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted}>
                     <p class="${isCompleted}">${todo.name}</p>
@@ -36,69 +37,83 @@ function showTodo(filter) {
                     </ul>
                 </div>
             </li>`;
-            }
-        });
-    }
-
-    taskBox.innerHTML = li || `<span>You don't have any task here</span>`;
+      }
+    });
+  }
+  // если li не пуст, вставляем это значение в поле задач, иначе span
+  taskBox.innerHTML = li || `<span>You don't have any task here</span>`;
 }
+
 showTodo("all");
 
+
+function getTaskView() {
+
+}
+
+
 function showMenu(selectedTask) {
-    let taskMenu = selectedTask.parentElement.lastElementChild;
-    taskMenu.classList.add("show");
-    document.addEventListener("click", e => {
-        if(e.target.tagName != "I" || e.target != selectedTask) {
-            taskMenu.classList.remove("show");
-        }
-    });
+  // получение меню задачи div
+  let taskMenu = selectedTask.parentElement.lastElementChild;
+  taskMenu.classList.add("show");
+  document.addEventListener("click", e => {
+    // удаление показа класса из меню задач при щелчке по документу
+    if (e.target.tagName !== "I" || e.target !== selectedTask) {
+      taskMenu.classList.remove("show");
+    }
+  });
 }
 
 function editTask(taskId, taskName) {
-    editId = taskId;
-    isEditedTask = true;
-    taskInput.value = taskName;
+  editId = taskId;
+  isEditedTask = true;
+  taskInput.value = taskName;
 }
 
 function deleteTask(deleteId) {
-    todos.splice(deleteId, 1);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo("all");
+  // удаление выбранной задачи из массива/todos
+  todos.splice(deleteId, 1);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo("all");
 }
 
 clearAll.addEventListener("click", () => {
-    todos.splice(0, todos.length);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo("all");
+  // удаление всех элементов массива/todos
+  todos.splice(0, todos.length);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo("all");
 });
 
 function updateStatus(selectedTask) {
-    let taskName = selectedTask.parentElement.lastElementChild;
-    if (selectedTask.checked) {
-        taskName.classList.add("checked");
-        todos[selectedTask.id].status = "completed";
-    } else {
-        taskName.classList.remove("checked");
-        todos[selectedTask.id].status = "pending";
-    }
-    localStorage.setItem("todo-list", JSON.stringify(todos));
+  // получение абзаца, содержащего название задачи
+  let taskName = selectedTask.parentElement.lastElementChild;
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    // обновление статуса выбранного задания до завершенного
+    todos[selectedTask.id].status = "completed";
+  } else {
+    taskName.classList.remove("checked");
+    // обновление статуса выбранной задачи на ожидающую
+    todos[selectedTask.id].status = "pending";
+  }
+  localStorage.setItem("todo-list", JSON.stringify(todos));
 }
 
 taskInput.addEventListener("keyup", e => {
-    let userTask = taskInput.value.trim();
-    if(e.key == "Enter" && userTask) {
-        if(!isEditedTask) {
-            if (!todos) {
-                todos = [];
-            }
-            let taskInfo = {name: userTask, status: "pending"};
-            todos.push(taskInfo); //adding new task to todos
-        } else {
-            isEditedTask = false;
-            todos[editId].name = userTask;
-        }
-        taskInput.value = "";
-        localStorage.setItem("todo-list", JSON.stringify(todos));
-        showTodo("all");
+  let userTask = taskInput.value.trim();
+  if (e.key === "Enter" && userTask) {
+    if (!isEditedTask) { // если isEditedTask не является истиной
+      if (!todos) { //если todos не существует, передаём пустой массив todos
+        todos = [];
+      }
+      let taskInfo = {name: userTask, status: "pending"};
+      todos.push(taskInfo); //добавление новой задачи в список todos
+    } else {
+      isEditedTask = false;
+      todos[editId].name = userTask;
     }
+    taskInput.value = "";
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo("all");
+  }
 });
